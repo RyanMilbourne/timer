e8d853a8366e311b71e84df08be7f39e2e10ba11
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-13 20:59:08 -0700

    refactored... a lot

--- timer1.js
+++ timer1.js
@@ -1,30 +1,10 @@
-const arguments = process.argv.slice(2);
-
 const timer = function() {
-
-  let count = [];
-
-  for (const arr of arguments) {
-    for (const val of arr) {
-      let num = parseInt(val);
-      if (num > 0) {
-        count.push(num);
-      }
+  for (const val of process.argv.slice(2)) {
+    if (parseInt(val) > 0) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, parseInt(val) * 1000);
     }
   }
-
-  const sortedCount = count.sort((a, b) => a - b);
-  const delays = [];
-
-  for (const val of sortedCount) {
-    delays.push(val * 1000);
-  }
-
-  for (const time of delays) {
-    setTimeout(() => {
-      process.stdout.write('\x07');
-    }, time);
-  }
-
 };
-timer(arguments);
+timer();
